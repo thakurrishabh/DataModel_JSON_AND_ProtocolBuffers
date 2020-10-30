88e5902936d6d9ec6e5462ebd7cdcafd7514f931
rishabh singh <thakur.rishabh.1997@gmail.com>
2020-10-29 20:20:21 -0400

    Final Working Version

--- Protocol Buffers/Assignment1_Client_ProtoBuff_TCP.py
+++ Protocol Buffers/Assignment1_Client_ProtoBuff_TCP.py
@@ -4,7 +4,7 @@ import socket
 def Main():
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('192.168.1.96', 4000)
+    server_address = ('35.183.148.239', 4000)
 
     s.connect(server_address)
     try:
